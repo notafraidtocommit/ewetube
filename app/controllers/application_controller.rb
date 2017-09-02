8065b01688a7873d50c5881d0185bb342f41b6ee
kentasy <kentasy@gmail.com>
2017-09-01 21:40:53 -0400

    Fixed bug with translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,25 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  
-  before_action :set_i18n_locale_from_params
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+protect_from_forgery with: :exception  
+before_action :set_i18n_locale_from_params
+
+
+
+
+
+
+
+
   protected
     def set_i18n_locale_from_params 
       if params[:locale]
         if I18n.available_locales.map(&:to_s).include?(params[:locale]) 
-          I18n.locale = params[:locale] 
+          I18n.locale = params[:locale]
         else
-          flash.now[:notice] =
-          "#{params[:locale]} translation not available"
-          logger.error flash.now[:notice] 
+            flash.now[:notice] =
+              "#{params[:locale]} translation not available"
+            logger.error flash.now[:notice] 
         end
-      end 
-    end  
-  
+      end
+    end
+
 end
